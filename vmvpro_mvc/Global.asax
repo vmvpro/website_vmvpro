ae918d542eddbb2d524804c62b67f4c9e2954a13
Vitaliy Melnik <melnvetal@gmail.com>
2019-03-27 00:37:32 +0200

    all comit

---
+++ vmvpro_mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="vmvpro_mvc.MvcApplication" Language="C#" %>
---
+++ vmvpro_mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(vmvpro_mvc.Startup))]
+namespace vmvpro_mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
